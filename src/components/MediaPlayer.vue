791d19fff60a2e10d11e62f0805bb22932a3a25a
David <chen7david@me.com>
2020-11-18 19:14:48 +0800

    update

---
+++ src/components/MediaPlayer.vue
@@ -0,0 +1,72 @@
+<template>
+    <v-sheet
+        :width="width"
+        :height="height"
+    >
+        <video
+            v-if="videos && videos[0]" 
+            crossorigin="anonymous"
+            controls
+            :key="videos[0].src" 
+            :poster="poster"  
+            :width="width" 
+            :height="height"
+        >
+            <source :src="getURL(videos[0].src)" type="video/mp4">
+                video can not be played in this browser.
+            <track
+                crossorigin="anonymous"
+                v-for="subtitle in subtitles" 
+                :key="subtitle.lang" 
+                :label="sublabel(subtitle.lang)" 
+                kind="captions" 
+                :srclang="subtitle.lang" 
+                :src="getURL(subtitle.src)" 
+                default
+            >
+        </video>
+    </v-sheet>
+</template>
+
+<script>
+export default {
+    name: 'MediaPlayer',
+    props: {
+        baseURL: null,
+        videos: null,
+        subtitles: null,
+        poster: null,
+        aspect: null,
+        width: null
+    },
+    computed: {
+        height(){
+            const [w,h] = this.aspect.split('/') 
+            return (this.width/w) * h
+        },
+    },
+    methods: {
+        sublabel(lang){
+            const map = {
+                en: 'English',
+                zh: 'Chinese',
+                nl: 'Dutch',
+                de: 'German',
+                fr: 'French',
+                sp: 'Spanish'
+            }
+            return map[lang] || 'unknown'
+        },
+        getURL(url){
+            console.log({url})
+            return this.baseURL ? this.baseURL + url : url
+        }
+    }
+}
+</script>
+
+<style scoped>
+video {
+  outline: none;
+}
+</style>
